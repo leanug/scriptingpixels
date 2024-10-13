d26db0b5ea95c914fe7fbbfcc1c61d2d9c2b97dc
Leandro Ubilla <leandroubilla@protonmail.com>
2024-10-13 16:08:48 -0300

    fix(home): Coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,20 +13,21 @@ export default function Home() {
       <div className="max-w-3xl mx-auto px-2.5">
         <div className="space-y-3 text-balance">
           <h1 className="text-xl md:text-5xl mt-2.5 font-semibold">
-            Code, One Step at a Time!
+            {/* Code, One Step at a Time! */}
+            Coming soon
           </h1>
-          <p className="text-2xl font-medium">
+          {/* <p className="text-2xl font-medium">
             Get actionable insights into coding, broken down by the numbers.
-          </p>
+          </p> */}
         </div>
         <div className="mt-8">
-          <h2 className="text-xl mb-2.5">Latest posts</h2>
+          {/* <h2 className="text-xl mb-2.5">Latest posts</h2>
           <PostsList posts={latestPosts} />
           <div className="w-full text-end mt-6">
             <Link className="btn btn-ghost text-primary" href="/blog">
               All posts &rarr;
             </Link>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
